e53ba2223004dc405047482006d86afa1d5af4fb
zklvyy <zklvyy@outlook.com>
2018-07-10 16:35:10 +0800

    add all vnpy function in ctp trader

--- trader_ctp/CtpTrader.cpp
+++ trader_ctp/CtpTrader.cpp
@@ -1,35 +1,36 @@
 #include "TraderSpi.h"
-#include "zhelpers.hpp"
-#include "ctp.pb.h"
+// #include "ctp.pb.h"
+// #include "zhelpers.hpp"
 
 Document d;
-zmq::context_t context (1);
-zmq::socket_t reply(context, ZMQ_REP);
-zmq::socket_t publisher(context, ZMQ_PUB);
+// zmq::context_t context(1);
+// zmq::socket_t reply(context, ZMQ_REP);
+// zmq::socket_t publisher(context, ZMQ_PUB);
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	GOOGLE_PROTOBUF_VERIFY_VERSION;
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-	if (argc != 2) {
-		cerr << "Usage: " << argv[0] << " ctp_config_file" << endl;
-		return -1;
-	}
+    if (argc != 2) {
+	cerr << "Usage: " << argv[0] << " ctp_config_file" << endl;
+	return -1;
+    }
 
-	string config_path = argv[1];
-	config_path = "./config/" + config_path;
-	std::cout << config_path.c_str() << std::endl;
-	 //if ( !read_json("ctp_zhongxin.json",d) ){
-	if ( !read_json(config_path.c_str(),d) ){
-		cout << "the config.json file parse error!" << endl;
-		return 0;
-	}
+    string config_path = argv[1];
+    config_path = "./config/" + config_path;
+    std::cout << config_path.c_str() << std::endl;
+    //if ( !read_json("ctp_zhongxin.json",d) ){
+    if (!read_json(config_path.c_str(), d)) {
+	cout << "the config.json file parse error!" << endl;
+	return 0;
+    }
 
-	logger::init(argv[0]);
-	publisher.bind("tcp://*:5541");
-	reply.bind("tcp://*:5531");
+    logger::init(argv[0]);
+    // publisher.bind("tcp://*:5541");
+    // reply.bind("tcp://*:5531");
 
-	CTraderSpi *ctraderapi = new CTraderSpi();
-	ctraderapi->load_config(d);
-	ctraderapi->connect();
+    CTraderSpi* ctraderapi = new CTraderSpi();
+    ctraderapi->load_config(d);
+    ctraderapi->connect();
 }
+
